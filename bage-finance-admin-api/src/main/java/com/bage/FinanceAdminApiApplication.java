5c2f616ee0b7bd555f8bc3d69454a10bd2746c91
NMDSKY <qinzw2000@163.com>
2024-03-06 20:01:20 +0800

    Sass系统项目初步搭建

---
+++ bage-finance-admin-api/src/main/java/com/bage/FinanceAdminApiApplication.java
@@ -0,0 +1,12 @@
+package com.bage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+public class FinanceAdminApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceAdminApiApplication.class, args);
+    }
+}
