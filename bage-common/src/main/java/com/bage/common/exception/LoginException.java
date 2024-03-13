151b41ab1a449e5f23701489638318155bcf8f50
NMDSKY <qinzw2000@163.com>
2024-03-13 18:59:22 +0800

    通用工具与前后端交互协议

---
+++ bage-common/src/main/java/com/bage/common/exception/LoginException.java
@@ -0,0 +1,19 @@
+package com.bage.common.exception;
+
+import com.bage.common.constant.ApiResponseCode;
+
+/**
+ * 登录异常
+ */
+public class LoginException extends BaseException {
+
+    private static final long serialVersionUID = 979094253305695687L;
+
+    public LoginException(String message) {
+        super(ApiResponseCode.LOGIN_ERROR.getCode(), message);
+    }
+
+    public LoginException(String message, Throwable t) {
+        super(message, t);
+    }
+}
